3f4d941c993c87ffba09ff404d369c670b96bc00
Artem <smirnov.artem.sam@gmail.com>
2021-05-22 15:38:08 +0400

    some fixing

--- src/main/java/ru/haazad/homework2/MainApp.java
+++ src/main/java/ru/haazad/homework2/MainApp.java
@@ -4,26 +4,32 @@ package ru.haazad.homework2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainApp {
     private static final Logger logger = LogManager.getLogger(MainApp.class);
 
-    private static final int ARRAY_SIZE = 1_000_000;
+    private static final int ARRAY_SIZE = 300_000;
 
     public static void main(String[] args) {
+        long startThreadTime = System.currentTimeMillis();
         Thread t1 = new Thread(() -> {
             Array<Integer> array = generateAndFillArray();
             logger.info("Current array size: " + array.size());
             array.sortBubble();
+            logger.info("Thread working time in seconds: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startThreadTime));
         });
         Thread t2 = new Thread(() -> {
             Array<Integer> array = generateAndFillArray();
             logger.info("Current array size: " + array.size());
             array.sortSelect();
+            logger.info("Thread working time in seconds: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startThreadTime));
         });
         Thread t3 = new Thread(() -> {
             Array<Integer> array = generateAndFillArray();
             logger.info("Current array size: " + array.size());
             array.sortInsert();
+            logger.info("Thread working time in seconds: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startThreadTime));
         });
         t1.start();
         t2.start();
